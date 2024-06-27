f9139b05b63f13d95ca07ae38bfd86d911845e4c
Lim JeongHu <dlaeocjf94@naver.com>
2024-06-27 18:40:08 +0900

    게시판 틀. 플젝 게시 목록 띄우기

--- src/pages/board/BoardList.js
+++ src/pages/board/BoardList.js
@@ -1,5 +1,67 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import AxiosApi from "../../api/AxiosApi";
+const List = styled.div``;
 const BoardList = () => {
-  return;
+  const [projectList, setProjectList] = useState(null);
+  const [boardList, setBoardList] = useState(null);
+  const [showProject, setShowProject] = useState(true);
+  const [showFree, setShowFree] = useState(false);
+  useEffect(() => {
+    const fetchProjectList = async () => {
+      try {
+        const rsp = await AxiosApi.getProjectList();
+        console.log(rsp.data);
+        setProjectList(rsp.data); // 필터링된 데이터를 상태에 저장
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchProjectList();
+  }, []);
+
+  useEffect(() => {
+    const fetchBoardList = async () => {
+      try {
+        const rsp = await AxiosApi.getBoardList();
+        console.log(rsp.data);
+        setBoardList(rsp.data); // 필터링된 데이터를 상태에 저장
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchBoardList();
+  }, []);
+  return (
+    <List
+      primary={showProject ? true : false}
+      onClick={() => {
+        setShowProject(true);
+        setShowFree(false);
+      }}
+    >
+      {showProject && (
+        <>
+          {projectList && (
+            <div>
+              {projectList.map((project, index) => (
+                <div key={index}>
+                  <div>
+                    <span>{project.projectName} ,</span>
+                    <span>{project.job},</span>
+                    <span>{project.projectTitle},</span>
+                    <span>{project.skills},</span>
+                    <span>{project.projectContent},</span>
+                  </div>
+                  <br></br>
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
+    </List>
+  );
 };
 export default BoardList;
--- src/pages/board/SelectBoard.js
+++ src/pages/board/SelectBoard.js
@@ -1,18 +1,59 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: row;
   width: 100%;
-  height: 80px;
-  background-color: #ff5353;
-  border-radius: 21px 21px 0 0;
+  height: 60px;
 `;
-const SelectButton = styled.button``;
+
+const SelectBoardButton = styled.button`
+  width: 50%;
+  height: auto;
+  border-radius: ${(props) =>
+    props.buttonType === "a" ? "30px 0 25px 0" : "0 30px 0 30px"};
+  border: none;
+  background-color: ${(props) => (props.isSelected ? "#ffffff" : "#ff5353")};
+  color: ${(props) => (props.isSelected ? "#000000" : "#ffffff")};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const SelectBoard = () => {
+  const [selectedButton, setSelectedButton] = useState({
+    a: true,
+    b: false,
+  });
+
+  const handleClick = (buttonType) => {
+    setSelectedButton((prevState) => ({
+      ...prevState,
+      [buttonType]: true,
+      [buttonType === "a" ? "b" : "a"]: false,
+    }));
+  };
+
   return (
     <Container>
-      <SelectButton></SelectButton>
+      <SelectBoardButton
+        isSelected={selectedButton.a}
+        buttonType="a"
+        onClick={() => handleClick("a")}
+      >
+        <span>자유 게시판</span>
+      </SelectBoardButton>
+      <SelectBoardButton
+        isSelected={selectedButton.b}
+        buttonType="b"
+        onClick={() => handleClick("b")}
+      >
+        <span>프로젝트 구인</span>
+      </SelectBoardButton>
     </Container>
   );
 };
+
 export default SelectBoard;

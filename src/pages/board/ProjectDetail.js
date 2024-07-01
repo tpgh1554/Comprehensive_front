77bf0d390f233b7183d6f437f71a5d9799616a67
limfarmer <dlaeocjf94@naver.com>
2024-07-01 21:33:54 +0900

    projectDetail css 기볼틀 생성

--- src/pages/board/ProjectDetail.js
+++ src/pages/board/ProjectDetail.js
@@ -1,6 +1,62 @@
 import { useEffect } from "react";
 import BoardLayout from "../../components/BoardLayout";
 import AxiosApi from "../../api/AxiosApi";
+import { Container, Top, ContentContainer } from "../../style/WriteStyle";
+import { HeadContainer } from "../../style/SelectBoardStyle";
+import {
+  Head,
+  UpHead,
+  UnderHead,
+  Body,
+  Footer,
+  ReplyContainer,
+  InputContainer,
+  UpInert,
+  UnderInert,
+  ReplyListContainer,
+  PageNum,
+} from "../../style/ProjectDetailStyle";
+import styled from "styled-components";
+const Title = styled.div``;
+const ProjectTime = styled.div``;
+const ProfileImg = styled.div`
+  /* Add your styles here */
+`;
+
+const RegDate = styled.div`
+  /* Add your styles here */
+`;
+
+const RecruitMemNum = styled.div`
+  /* Add your styles here */
+`;
+
+const Content = styled.div`
+  /* Add your styles here */
+`;
+
+const Skills = styled.div`
+  /* Add your styles here */
+`;
+
+const ListBtt = styled.div`
+  /* Add your styles here */
+`;
+
+const Input = styled.div`
+  /* Add your styles here */
+`;
+
+const ConfirmReply = styled.div`
+  /* Add your styles here */
+`;
+
+const NickName = styled.div`
+  /* Add your styles here */
+`;
+const ReplyContent = styled.div`
+  /* Add your styles here */
+`;
 const ProjectDetail = () => {
   useEffect(() => {
     const projectDetail = async (id) => {
@@ -14,6 +70,51 @@ const ProjectDetail = () => {
     };
     projectDetail();
   }, []);
-  return <BoardLayout></BoardLayout>;
+  return (
+    <BoardLayout>
+      <Container>
+        <HeadContainer style={{ justifyContent: "center" }}>
+          <Top>프로젝트</Top>
+        </HeadContainer>
+        <ContentContainer>
+          <Head>
+            <UpHead>
+              <Title></Title>
+              <ProjectTime></ProjectTime>
+            </UpHead>
+            <UnderHead>
+              <ProfileImg></ProfileImg>
+              <RegDate></RegDate>
+              <RecruitMemNum></RecruitMemNum>
+            </UnderHead>
+          </Head>
+          <Body>
+            <Content></Content>
+            <Skills></Skills>
+            <ListBtt></ListBtt>
+          </Body>
+          <Footer></Footer>
+        </ContentContainer>
+        <ReplyContainer>
+          <InputContainer>
+            <UpInert>
+              <ProfileImg></ProfileImg>
+              <Input></Input>
+            </UpInert>
+            <UnderInert>
+              <ConfirmReply></ConfirmReply>
+            </UnderInert>
+          </InputContainer>
+          <ReplyListContainer>
+            <ProfileImg></ProfileImg>
+            <NickName></NickName>
+            <ReplyContent></ReplyContent>
+            <RegDate></RegDate>
+          </ReplyListContainer>
+          <PageNum></PageNum>
+        </ReplyContainer>
+      </Container>
+    </BoardLayout>
+  );
 };
 export default ProjectDetail;
---
+++ src/style/ProjectDetailStyle.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const UpHead = styled.div`
+  /* Add your styles here */
+`;
+
+export const UnderHead = styled.div`
+  /* Add your styles here */
+`;
+
+export const Body = styled.div`
+  flex: 1;
+  /* Add your styles here */
+`;
+
+export const Footer = styled.div`
+  /* Add your styles here */
+`;
+
+export const ReplyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UpInert = styled.div`
+  /* Add your styles here */
+`;
+
+export const UnderInert = styled.div`
+  /* Add your styles here */
+`;
+
+export const ReplyListContainer = styled.div`
+  /* Add your styles here */
+`;
+
+export const PageNum = styled.div`
+  /* Add your styles here */
+`;
